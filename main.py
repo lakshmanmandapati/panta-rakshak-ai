4697052ee6c11f1fdcd437036f182e98b71f2975
Lakshman Mandapati <lakshmanmandapati0@gmail.com>
2025-07-13 12:10:41 +0530

    main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ app = FastAPI(title="Potato Disease Classifier API")
 origins = [
     "http://localhost",
     "http://localhost:3000",
+    "http://localhost:5173",
 ]
 app.add_middleware(
     CORSMiddleware,
